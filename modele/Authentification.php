1c83ba0f900f106ca6031b0bf4a3714e3f2851c0
MaurrouViff <rofldu08@gmail.com>
2025-01-21 14:53:19 +0100

    Ajout de la connexion & inscription

--- modele/Authentification.php
+++ modele/Authentification.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 class Authentification {
     private static function getFilePath(): string {
-        // Chemin vers le fichier JSON
+
         return dirname(__FILE__) . '/users.json';
     }
 
@@ -10,14 +10,14 @@ class Authentification {
         // Lecture du fichier JSON
         $filePath = self::getFilePath();
         if (!file_exists($filePath)) {
-            return ['users' => []]; // Retourne une structure vide si le fichier n'existe pas
+            return ['users' => []];
         }
         $jsonContent = file_get_contents($filePath);
         return json_decode($jsonContent, true);
     }
 
     private static function writeJson(array $data): void {
-        // Écriture dans le fichier JSON
+
         $filePath = self::getFilePath();
         file_put_contents($filePath, json_encode($data, JSON_PRETTY_PRINT));
     }
@@ -25,14 +25,14 @@ class Authentification {
     public static function register(string $nom, string $motDePasse): string {
         $data = self::readJson();
 
-        // Vérification si l'utilisateur existe déjà
+
         foreach ($data['users'] as $user) {
             if ($user['nom'] === $nom) {
                 return "Cet utilisateur existe déjà.";
             }
         }
 
-        // Création d'un nouvel utilisateur
+
         $newUser = [
             "id" => count($data['users']) + 1,
             "nom" => $nom,
@@ -42,7 +42,7 @@ class Authentification {
         ];
         $data['users'][] = $newUser;
 
-        // Écriture dans le fichier JSON
+
         self::writeJson($data);
 
         return "Inscription réussie.";
@@ -51,7 +51,7 @@ class Authentification {
     public static function login(string $nom, string $motDePasse): string {
         $data = self::readJson();
 
-        // Recherche de l'utilisateur
+
         foreach ($data['users'] as $user) {
             if ($user['nom'] === $nom && $user['mot_de_passe'] === $motDePasse) {
                 return "Connexion réussie.";
