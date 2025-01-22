b06878615a546657da4441d00810a5ecbd9b8a04
KAYRAZZ <valentin.chigot@orange.fr>
2025-01-22 12:03:45 +0100

    update

--- modele/SearchMovieModel.php
+++ modele/SearchMovieModel.php
@@ -11,7 +11,7 @@ use GuzzleHttp\Exception\GuzzleException;
 class SearchMovieModel
 {
     private Client $client;
-    private mixed $apiKey;
+    private string $apiKey;
 
     public function __construct()
     {
@@ -23,10 +23,14 @@ class SearchMovieModel
     }
 
     /**
+     * Search for movies using the API.
+     *
+     * @param string $query
+     * @return string
      * @throws GuzzleException
      * @throws Exception
      */
-    public function searchMovie(string $query): false|string
+    public function searchMovie(string $query): string
     {
         $response = $this->client->request('GET', 'https://api.themoviedb.org/3/search/movie', [
             'query' => [
@@ -42,7 +46,7 @@ class SearchMovieModel
             'verify' => false, // Disable SSL verification
         ]);
 
-        $movies = json_decode($response->getBody(), true);
+        $movies = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
 
         // Check if the required fields are present
         foreach ($movies['results'] as $movie) {
@@ -52,6 +56,6 @@ class SearchMovieModel
         }
 
         // Return the results as JSON
-        return json_encode(['results' => $movies['results']]);
+        return json_encode(['results' => $movies['results']], JSON_THROW_ON_ERROR);
     }
 }
